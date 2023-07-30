2f4b0ec5ad085897dffc51c986a4dab518f57f38
devisettymanikanta <42551150+devisettymanikanta@users.noreply.github.com>
2023-07-30 10:41:39 +0530

    Update zip_job.py

--- zip_job.py
+++ zip_job.py
@@ -2,17 +2,17 @@ import os
 import sys
 import zipfile
 
-# a. Create an array of a, b, c, d, e, f....z
+# Create an array of a, b, c, d, e, f....z
 alphabet = [chr(ord('a') + i) for i in range(26)]
 
-# b. Based on this array, create txt files (a.txt, b.txt....)
+# Based on this array, create txt files (a.txt, b.txt....)
 def create_txt_files():
     for letter in alphabet:
         filename = f"{letter}.txt"
         with open(filename, 'w') as file:
             file.write("This is a sample text for " + filename)
 
-# c. Make sure all txt files are created and if not - fail the script
+# Make sure all txt files are created and if not - fail the script
 def verify_txt_files():
     for letter in alphabet:
         filename = f"{letter}.txt"
@@ -20,7 +20,7 @@ def verify_txt_files():
             print(f"ERROR: File '{filename}' is missing.")
             sys.exit(1)
 
-# d. Create zip files with names based on array + VERSION environment variable, that each one will have one txt file inside
+# Create zip files with names based on array + VERSION environment variable, that each one will have one txt file inside
 def create_zip_files(version):
     for letter in alphabet:
         filename = f"{letter}.txt"
@@ -28,7 +28,7 @@ def create_zip_files(version):
         with zipfile.ZipFile(zip_filename, 'w') as zip_file:
             zip_file.write(filename)
 
-# e. Make sure all zip files are created and if not - fail the script
+# Make sure all zip files are created and if not - fail the script
 def verify_zip_files(version):
     for letter in alphabet:
         zip_filename = f"{letter}_{version}.zip"
@@ -45,16 +45,16 @@ if __name__ == "__main__":
 
     print(f"Running zip_job.py with VERSION={version}")
 
-    # b. Based on this array, create txt files (a.txt, b.txt....)
+    # Based on this array, create txt files (a.txt, b.txt....)
     create_txt_files()
 
-    # c. Make sure all txt files are created
+    # Make sure all txt files are created
     verify_txt_files()
 
-    # d. Create zip files with names based on array + VERSION environment variable
+    # Create zip files with names based on array + VERSION environment variable
     create_zip_files(version)
 
-    # e. Make sure all zip files are created
+    # make sure all zip files are created
     verify_zip_files(version)
 
     print("All operations completed successfully.")
